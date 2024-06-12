2a95f74ecad66750b0fb3097beae0372a6af8d7a
SYEDA ARBISH <jugnoo708@gmail.com>
2024-06-12 07:31:32 +0500

    Add files via upload

---
+++ assig.py/work.py/SA.py
@@ -0,0 +1,5 @@
+side = 20
+
+
+
+print("area of squre is",side*side)
---
+++ assig.py/work.py/TA.py
@@ -0,0 +1,11 @@
+height = float(input(" enter the height of traingle:   "))
+Base = float(input("enter the base of traingle:    "))
+
+
+
+area =(1/2)*height*Base 
+
+
+
+
+print("the area of traingle is",area)
